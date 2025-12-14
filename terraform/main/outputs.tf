ca9d7ceed2b0668193253a80cd0d14abf5a7d6f0
Marvin Tank <marvintank01@gmail.com>
2025-12-14 22:05:44 +0100

    added sensitive to outputs

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -1,36 +1,50 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
 output "public_subnet_id" {
   value = aws_subnet.public.id
 }
+
 output "private_subnet_id" {
   value = aws_subnet.private.id
 }
+
 output "private_subnet_b_id" {
   value = aws_subnet.private_b.id
 }
+
 output "web_sg_id" {
   value = aws_security_group.web_sg.id
 }
+
 output "db_sg_id" {
   value = aws_security_group.db_sg.id
 }
+
 output "db_endpoint" {
-  value = aws_db_instance.mysql.address
+  value     = aws_db_instance.mysql.address
+  sensitive = true
 }
+
 output "db_port" {
   value = aws_db_instance.mysql.port
 }
+
 output "db_name" {
-  value = aws_db_instance.mysql.db_name
+  value     = aws_db_instance.mysql.db_name
+  sensitive = true
 }
+
 output "db_username" {
-  value = aws_db_instance.mysql.username
+  value     = aws_db_instance.mysql.username
+  sensitive = true
 }
+
 output "static_bucket_name" {
   value = aws_s3_bucket.static_site.bucket
 }
+
 output "static_bucket_website_endpoint" {
   value = aws_s3_bucket_website_configuration.static_site.website_endpoint
 }
